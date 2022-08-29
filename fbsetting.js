746df1131921dd5e7ee340aeef42ff7901a26c3a
hoonsw23 <porkpitch@naver.com>
2022-08-29 21:53:09 +0900

    20220829

---
+++ fbsetting.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, set, update, ref, onValue } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD3rq7xxnofkGwajV6D62BXGJ4WfF0MIMc",
+  authDomain: "fbsimplescraper.firebaseapp.com",
+  databaseURL: "https://fbsimplescraper-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "fbsimplescraper",
+  storageBucket: "fbsimplescraper.appspot.com",
+  messagingSenderId: "865553940068",
+  appId: "1:865553940068:web:b760ad2a07b362dc796971",
+  measurementId: "G-WZPW4DCQGW"
+};
+
+// initialize firebase
+export const app = initializeApp(firebaseConfig);
+
+// export && initialize realtime database
+export const database = getDatabase(app);
--- simplescraper.js
+++ simplescraper.js
@@ -1,45 +1,57 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase, set, update, ref } from "firebase/database";
+import { set, ref, onValue } from "firebase/database";
+import { database } from './fbsetting.js';
 import axios from 'axios';
 import cheerio from 'cheerio';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyD3rq7xxnofkGwajV6D62BXGJ4WfF0MIMc",
-    authDomain: "fbsimplescraper.firebaseapp.com",
-    databaseURL: "https://fbsimplescraper-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "fbsimplescraper",
-    storageBucket: "fbsimplescraper.appspot.com",
-    messagingSenderId: "865553940068",
-    appId: "1:865553940068:web:b760ad2a07b362dc796971",
-    measurementId: "G-WZPW4DCQGW"
-  };
-
-// initialize firebase
-const app = initializeApp(firebaseConfig);
-
-// export && initialize realtime database
-const database = getDatabase(app);
-
 let today = new Date();
 
-axios.get('https://gall.dcinside.com/board/lists?id=bitcoins_new1')
-    .then((htmlParse) => {
-        const $ = cheerio.load(htmlParse.data);
-        let arr =[];
+let url = 'https://gall.dcinside.com/board/lists?id=bitcoins_new1';
+let repeattime = 1000* 60 * 5; // 5분
+let counttime = 0;
+let endtime = 1000* 60 * 60 * 24; // 24시간
+let array = [];
 
-        $('tbody tr').each((index, element)=>{
+const starCountRef = ref(database, 'dcdata');
 
-            arr.push({
-                num : $(element).find('.gall_num').text(),
-                title : $(element).find('a').text().trim(),
-                time : $(element).find('.gall_date').text(),
-                date : today.toLocaleDateString()
-            });
-        });
+var interval = setInterval(function(){
+    let lastnum = 0;
+    let array2 =[];
 
-        set(ref(database,'dcdata'),arr);
-
+    onValue(starCountRef, (snapshot) => {
+        array =snapshot.val();
+        lastnum =array[0].num;
     });
 
-app.delete()
+    axios.get(url)
+    .then((htmlParse) => {
+        const $ = cheerio.load(htmlParse.data);
+
+        $('tbody tr').each((index, element)=>{
+            if(index>= 5){ //공지글 제거
+                array2.push({
+                    num : $(element).find('.gall_num').text(),
+                    title : $(element).find('a').text().trim(),
+                    time : $(element).find('.gall_date').text(),
+                    date : today.toLocaleDateString()
+                });
+
+            }
+        });
+        array2.forEach((item, idx)=>{
+            if(item.num > lastnum){
+                array.push(item);
+                console.log(item); // for test output
+            }
+        })
+        array.sort(function(a, b){
+            return b.num - a.num;
+        });
+        lastnum=array.length;
+        set(ref(database,'dcdata'),array);
+    });
+    counttime += repeattime;
+    if(counttime > endtime) { clearInterval(interval);};
+}, repeattime);
+
+//app.delete()
 
